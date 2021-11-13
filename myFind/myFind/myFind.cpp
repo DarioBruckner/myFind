91bc8d8db3e8f90ca30499e3ab2c4d08b8cb9999
Linus Dehner <if21b132@technikum-wien.at>
2021-11-13 19:04:22 +0100

    added getopt and forking functionality

--- myFind/myFind/myFind.cpp
+++ myFind/myFind/myFind.cpp
@@ -3,19 +3,32 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <string>
+
 /* Hilfsfunktion */
 void print_usage( char *programm_name ) {
     printf("Usage: %s [-R] [-i] searchpath filename1 [filename2] ... [filenameN]\n\n",programm_name);
     return;
 }
 
+void child(int argc, char **argv, int index, std::string path, bool rec, bool insensitive)
+{
+    pid_t pid;
+    // Suchlogik einbauen
+    printf("%s\n",argv[index]);
+}
+
 int main(int argc, char*argv[] )
 {
     int c;
+    // Command und Path
+    int param_count = 2;
     char *programm_name;
+    pid_t pid;
+    bool rec = false;
+    bool insensitive = false;
     programm_name = argv[0];
-
-    while ((c = getopt(argc, argv, "Ri:")) != EOF){
+    while ((c = getopt(argc, argv, "Ri")) != EOF){
         switch (c)
         {
         case '?':
@@ -23,13 +36,43 @@ int main(int argc, char*argv[] )
             exit(1);
             break;
         case 'R':
+            rec = true;
+            printf("R\n");
+            param_count++;
             /* code */
             break;
         case 'i':
+            insensitive = true;
+            printf("i\n");
+            param_count++;
             break;
         default:
+            print_usage(programm_name);
             break;
         }
     }
-    std::cout << "Hey World!\n";
+    
+    /* Geht die angegebenen Files durch und forkt für jedes File */
+    if(param_count < argc){
+        for(int i = param_count; i < argc; i++){
+        pid = fork();
+        switch (pid)
+	    {
+            case -1:
+                printf("Child konnte nicht gestartet werden.");
+                exit(EXIT_FAILURE);
+                break;
+            case 0:
+                printf("child\n");
+                child(argc, argv, i, argv[param_count-1], rec, insensitive);
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                break;
+	    }
+    }
+    }else{
+        print_usage(programm_name);
+    }
+    
 }
